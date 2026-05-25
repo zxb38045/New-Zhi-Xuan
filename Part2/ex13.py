2af922d3501c4efcd884223ea4d16d34122340c7
zxb38045 <newzhixuan@gmail.com>
2026-05-25 05:22:03 +0000

    MUFY Plus Part 2

---
+++ Part2/ex13.py
@@ -0,0 +1,11 @@
+# Start from zero
+count = 0
+
+# TODO:
+# Create a while loop that prints the count
+# Stop the loop when count reaches 5
+
+
+# Insert your code here
+
+# Don't forget to increase count each loop!
